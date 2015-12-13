e369d4e935ac3b68a31cb2d58e32bdc0aba144d9
allonzy <simviv28@hotmail.fr>
2015-12-13 07:49:45 +0100

    s

--- tamaCore/aliment.py
+++ tamaCore/aliment.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 # coding=utf-8
+aliment_satiety, aliment_happyness, aliment_weight
 
-aliment_list(satiety, happyness, weight)
+aliment_list = dict()
+aliment_list{
+"Cake": [1,5,2.5],
+"Croquette":[2,-1,1]
+}
