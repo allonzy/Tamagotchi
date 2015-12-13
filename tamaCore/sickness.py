4af3f50336ccebcf303409efc2b8614ebcbd5df1
allonzy <simviv28@hotmail.fr>
2015-12-13 04:47:01 +0100

    add the sickness effect

--- tamaCore/sickness.py
+++ tamaCore/sickness.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
 # coding=utf-8
+"""sickness_list[stat]=(sickness_stat,sickness_statValue,sickness_degen)"""
+sickness_list = {
+"Hungry": ("satiety",20, {"health" = -0.2 } ),
+"Very Hungry":("satiety",0, {"health" = -0.46 } ),
+"Tired": ("energy",20, {"happyness" = -0.2 } ),
+"Very Tired": ("energy",0, {"happyness" = -0.46 } ),
+"Dirty": ("cleanness",20,{"happyness" = -0.2 } ),
+"Very Dirty": ("cleanness",0,{"happyness" = -0.2 } )
+"Depressive": ("happyness",20,{ "happyness": -0.5 ,"health": -0.5 } )
+"Very Depressive":("happyness",0,{"happyness": -0.5,"health": -0.5})
+}
