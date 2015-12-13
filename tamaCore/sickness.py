d5d65e8910ba3352aab508366fc54a3713fc50fa
allonzy <simviv28@hotmail.fr>
2015-12-13 06:26:52 +0100

    t

--- tamaCore/sickness.py
+++ tamaCore/sickness.py
@@ -3,14 +3,17 @@
 """sickness_list[stat]=(sickness_stat,sickness_statValue,sickness_degen)"""
 sickness_list = dict()
 sickness_list = {
-"Healthy",
-"Hungry": ("satiety",20, {"health" = -0.2 } ),
-"Very Hungry":("satiety",0, {"health" = -0.46 } ),
-"Tired": ("energy",20, {"happyness" = -0.2 } ),
-"Very Tired": ("energy",0, {"happyness" = -0.46 } ),
-"Dirty": ("cleanness",20,{"happyness" = -0.2 } ),
-"Very Dirty": ("cleanness",0,{"happyness" = -0.2 } )
-"Depressive": ("happyness",20,{ "happyness": -0.5 ,"health": -0.5 } )
-"Very Depressive":("happyness",0,{"happyness": -0.5,"health": -0.5} )
-""
+"Healthy": ["health",100, {} ],
+"Hungry": ["satiety",20, {"health" = -0.2 } ],
+"Very Hungry":["satiety",0, {"health" = -0.46 } ],
+"Tired": ["energy",20, {"happyness" = -0.2 } ],
+"Very Tired": ["energy",0, {"happyness" = -0.46 } ],
+"Dirty": ["cleanness",20,{"happyness" = -0.2 } ],
+"Very Dirty": ["cleanness",0,{"happyness" = -0.2 } ],
+"Depressive": ["happyness",20,{ "happyness": -0.5 ,"health": -0.5 } ],
+"Very Depressive":["happyness",0,{"happyness": -0.5,"health": -0.5} ],
+"Skinny":["weight",30,{"energy": -0.2}],
+"Very skinny":["weight",10,{"energy": -0.46}],
+"Fat":["weight",70,{"energy": -0.2}],
+"Very fat":["weight",90,{"energy": -0.2}],
 }
